eb2336b2e41ac11ef9a35b6281cb3fc95bb2e38b
David Koblas <david@tubularlabs.com>
2018-08-24 16:00:28 -0400

    updated intent

--- amazon/intents.go
+++ amazon/intents.go
@@ -1,20 +1,10 @@
 package amazon
 
 var (
-	// AMAZON Skill Intents
-	AMAZON = struct {
-		StartOverIntent string
-		CancelIntent    string
-		PauseIntent     string
-		HelpIntent      string
-		StopIntent      string
-		RepeatIntent    string
-	}{
-		StartOverIntent: "AMAZON.StartOverIntent",
-		CancelIntent:    "AMAZON.CancelIntent",
-		PauseIntent:     "AMAZON.PauseIntent",
-		HelpIntent:      "AMAZON.HelpIntent",
-		StopIntent:      "AMAZON.StopIntent",
-		RepeatIntent:    "AMAZON.RepeatIntent",
-	}
+	StartOverIntent = "AMAZON.StartOverIntent"
+	CancelIntent    = "AMAZON.CancelIntent"
+	PauseIntent     = "AMAZON.PauseIntent"
+	HelpIntent      = "AMAZON.HelpIntent"
+	StopIntent      = "AMAZON.StopIntent"
+	RepeatIntent    = "AMAZON.RepeatIntent"
 )
